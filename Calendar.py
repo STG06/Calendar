173850944c7f241beaa6917a1a0e55da23b5a6ad
STG06 <100060298+STG06@users.noreply.github.com>
2025-01-08 08:35:10 -0900

    Update Calendar.py

--- Calendar.py
+++ Calendar.py
@@ -1,6 +1,6 @@
 
 import streamlit as st
-from streamlit_calendar import calendar
+from streamlit_calendar import Calendar
 
 st.title("Suleyman's Calendar")
 
@@ -22,7 +22,7 @@ with st.form("Add Event"):
         st.session_state.events.append(new_event)
         st.success(f"Event '{title}' added successfully!")
 
-calendar1 = calendar(
+calendar1 = Calendar(
     initial_view="dayGridMonth",
     events=st.session_state.events,
     default_date="2025-01-01",  # Start the calendar on January 2025
